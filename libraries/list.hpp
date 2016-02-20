f98b922d0a9f979f7869704b5324594b0bb99bf0
sdg31 <sdg31@zips.uakron.edu>
2016-02-20 11:44:07 -0500

    Update list.hpp

--- libraries/list.hpp
+++ libraries/list.hpp
@@ -5,6 +5,9 @@
 
 #include <iostream>
 
+//this singly linked list has two named nodes, a head and tail
+//these are not dummy nodes and both contain data
+//tail->next should always be nullptr
 template<typename T>
 class List
 {
@@ -22,26 +25,33 @@ public:
   void print();  
 };
 
+//default constructor creates empty list
 template<typename T>
 List<T>::List()
   :head(), tail()
 {
 }
 
+//insert a new head into the list
 template<typename T>
 void List<T>::push_front( T data )
-{   
+{ 
+  //if the list is empty, allocate a new head
+  //since there is only one element, the head is also the tail
   if( !head )
   {
     head = new Node<T>(data, nullptr);
     tail = head;
   }
+  //since the list is linked singly, there is only one special case (the list is empty),
+  //so otherwise, simply create a new head and set it's next pointer to the previous head
   else
   {
     head = new Node<T>(data, head);
   }
 }
 
+//push_back is fundamentally the same as push_front
 template<typename T>
 void List<T>::push_back( T data )
 {   
@@ -57,17 +67,24 @@ void List<T>::push_back( T data )
   }
 }
 
+//delete the head of the list
 template<typename T>
 void List<T>::pop_front()
 {
+  //if there is no head, this is a logic error
+  //we have made such an error impossible, although this is optional
   if ( !head )
       return;
-
+  
+  //if there is only one element (head == tail), then simply delete it
+  //and set both pointers to nullptr
   if( head == tail)
   {
     delete head;
     head = tail = nullptr;
   }
+  //in all other cases, a temporary node must be created that copies the second node in the list
+  //the head is deleted, and temp (the second node in the list), is made into the head
   else
   {
     Node<T>* temp = head->next;
@@ -76,9 +93,12 @@ void List<T>::pop_front()
   }
 }
 
+//prints the contents of every node in the list
 template<typename T>
 void List<T>::print()
 {
+  //a temporary node is created, and while it is not null, it's data is printed
+  //when (temp == tail->next), it will be null, since tail->next is always a nullptr
   Node<T>* temp = head;
   while( temp ) {
     std::cout << temp->data << ' ';
@@ -90,6 +110,7 @@ void List<T>::print()
 template<typename T>
 List<T>::~List()
 {
+  //iterate through every element and remove it until nothing is left
   while( head )
     this->pop_front();
 }

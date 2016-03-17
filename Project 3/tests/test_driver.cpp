92e112f26996147ade5331ef943908d7c74f7e04
sdg31 <sdg31@zips.uakron.edu>
2016-03-16 23:48:05 -0400

    comprehensive driver test (albeit bad unit test)

--- Project 3/tests/test_driver.cpp
+++ Project 3/tests/test_driver.cpp
@@ -2,12 +2,28 @@
 #include "shell_sort.hpp"
 #include <vector>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
-	std::vector<int> jason = { 3, 6, 400, 4, 9, 53, 3, 2, 1, 842 };
-	shell_sort( jason );
-	for( int i = 0; i < jason.size(); i++ )
-		std::cout << jason[i] << std::endl;
+	srand( time( nullptr ) );
+	std::vector<int> jake;
+	for( int i = 0; i < (rand() % 100); i++ )
+	{
+		jake.push_back( rand() % 10000 );
+	}
+	std::cout << "Jake the vector has " << jake.size() << " elements and is defined as:\n";
+	for( int i = 0; i < jake.size(); i++ )
+		std::cout << jake[i] << " ";
+	std::cout << "\n";
+
+	shell_sort( jake );
+
+	std::cout << "Jake has been sorted. He was reluctant to start, but in the end he's glad he went through with it.\n";
+	std::cout << "Jake is now defined as:\n";
+	for( int i = 0; i < jake.size(); i++ )
+		std::cout << jake[i] << " ";
+	std::cout << "\n";
 	return 0;
 }

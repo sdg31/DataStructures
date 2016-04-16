694843e2153fa172c7feac40e393562f4ac37ce1
sdg31 <sdg31@zips.uakron.edu>
2016-04-16 15:07:18 -0400

    log2 is now fast

--- Project 3/libraries/shell_sort.hpp
+++ Project 3/libraries/shell_sort.hpp
@@ -32,13 +32,12 @@ void shell_sort( std::vector<T> &array )
 
 // an optimization for shellsort is to create an offset that is the highest 
 // power of 2 less than the capacity. this function finds the offset
-// by first calculating the log base 2 of the capacity, subtracting one
-// from it, and then computing 2^x
+// by first calculating the log base 2 of the capacity, and then computing 2^x
 template<typename T>
 void shell_sort_log_2( std::vector<T> &array )
 {
         int capacity = array.size();
-	int offset_power = log2( capacity ) - 1;
+	int offset_power = log2( capacity );
 	int offset = pow( 2, offset_power );
 	while( offset_power > 0 )
 	{
@@ -51,7 +50,41 @@ void shell_sort_log_2( std::vector<T> &array )
 					swap( array[i], array[i+offset] );
 			}
 		}
-		--offset_power;
+		offset_power -= 1;
 	}
 	insertion_sort( array );
 }
+
+//custom log 2 using built in gcc functions
+//finds size of T in bytes, multiplies by 8 to get the bits
+//subtracts the number of leading zeroes in x, and then a subtraction by one
+//ONLY WORKS WITH GCC
+template<typename T>
+int clog2( int x )
+{
+    return (sizeof(T)*8-__builtin_clz(x))-1;
+}
+
+//shellsort log2 using clog2
+template<typename T>
+void shell_sort_clog2( std::vector<T> &array )
+{
+        int capacity = array.size();
+	int offset_power = clog2<int>( capacity );
+	int offset = pow( 2, offset_power );
+	while( offset_power > 0 )
+	{
+	        offset = pow( 2, offset_power );    
+		for( int i = 0; i < capacity; i++ )
+		{
+			if( i + offset <= capacity )
+			{
+				if( array[i] > array[i + offset] )
+					swap( array[i], array[i+offset] );
+			}
+		}
+		offset_power -= 1;
+	}
+	insertion_sort( array );
+}
+

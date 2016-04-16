c865b8317a223460fffd1e7ef32778bac0ffb265
sdg31 <sdg31@zips.uakron.edu>
2016-04-16 14:36:58 -0400

    stopwatch of various shellsort optimizations

---
+++ Project 3/tests/test1.cpp
@@ -0,0 +1,40 @@
+#include "../libraries/shell_sort.hpp"
+#include "../libraries/stopwatch.hpp"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    StopWatch shell_sw;
+    StopWatch log2_sw;
+    StopWatch clog2_sw;
+
+    srand( time( nullptr ) );
+    std::vector<int> test1;
+    std::vector<int> test2;
+    std::cout << "mk1\n";
+    for( int i = 0; i < 100000; i++ )
+    {
+	test1.push_back( rand() % 10000 );
+    }
+    std::cout << "mk2\n";
+    test2 = test1;
+    std::cout << "mk3\n";
+    
+    std::cout <<  __builtin_clz( test2.size() ) << '\n';
+
+    shell_sw.start();
+    shell_sort( test1 );
+    std::cout << "n/2 shellsort took " << shell_sw.pause() << " ms\n";
+
+    log2_sw.start();
+    shell_sort_log_2( test2 );
+    std::cout << "log2 shellsort took " << log2_sw.pause() << " ms\n";
+
+    clog2_sw.start();
+    shell_sort_clog2( test2 );
+    std::cout << "clog2 shellsort took " << clog2_sw.pause() << " ms\n";
+
+}
